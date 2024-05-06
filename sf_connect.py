0bc735c7928936ea9f51e1c991fa04a0d3191b68
Daniel Gallagher <danielg@mozillafoundation.org>
2024-05-06 12:50:02 -0400

    Removed extraneous code that was integrated elsewhere.

--- sf_connect.py
+++ sf_connect.py
@@ -57,33 +57,10 @@ class sf_connect:
                                     ,password = self.__credentials.get('SALESFORCE_API_PASS')
                                     ,security_token = self.__credentials.get('SALESFORCE_API_TOKEN')
                                     ,session_id = requests.Session())
-    
-    #Create the method to run a SOQL query against the Salesforce connection
-    def sf_query_to_df(self, query):
-        try:
-            #If the query string was provided then execute the query
-            if query:
-                query = format_soql(query)
-                query_results = self.salesforce.query_all(query)
-            
-            #Otherwise raise an exception
-            else:
-                raise Exception('The value of query cannot be null')
-            
-            #If query results were retrieved transform the dictionary into a dataframe
-            if query_results:
-                results_df = (pd.DataFrame([r for r in query_results.get('records')])
-                                          .drop(columns = 'attributes'))
-            #Return the dataframe
-            return results_df
-        
-        #Otherwise raise any exceptions from the Salesforce class or otherwise
-        except Exception as e:
-            raise(e)
         
     def get_sf_object(self, query):
         if query:
-            query = format_soql(query)
+            self.query = format_soql(query)
 
             #Create the search expression: the "from" statement, with spaces and at least one of any letter or digit.
             #The purpose is to extract the Salesforce Object
@@ -103,30 +80,26 @@ class sf_connect:
             except:
                 raise Exception('No salesforce object found in query. The result is empty, please check your query')
             
-    def sf_object_dtypes(self, query):
-        self.get_sf_object(query)
-
-        #Create a dict of column names and the salesforce data types
-        from_dtypes = {c.get('name'): c.get('type') for c in self.salesforce.__getattr__(self.sf_object).describe().get('fields')}
-
-        #Map these data types to the appropriate pandas data types
-        self.to_dtypes = {k: DTYPE_MAPPER.get(v) for k,v in from_dtypes.items()}
-        #return to_dtypes
-    
     def sf_query_object(self, query):
-        
+
+        #Add the salesforce object attribute from the query
         self.get_sf_object(query)
+        
+        try:
+            #Create the connections to the appropriate endpoints
+            self.api_object = self.salesforce.__getattr__(self.sf_object)
+            self.bulk2_object = self.salesforce.bulk2.__getattr__(self.sf_object)
 
-        self.api_object = self.salesforce.__getattr__(self.sf_object)
-        self.bulk2_object = self.salesforce.bulk2.__getattr__(self.sf_object)
+            #Create a dict of column names and the salesforce data types
+            from_dtypes = {c.get('name'): c.get('type') for c in self.api_object.describe().get('fields')}
+    
+            #Connect to the object via bulk2
+            results = self.bulk2_object.query(self.query)
 
-        #Create a dict of column names and the salesforce data types
-        from_dtypes = {c.get('name'): c.get('type') for c in self.api_object.describe().get('fields')}
-
-        #Connect to the object via bulk2
-        results = self.bulk2_object.query(query)
-
-
+        #Otherwise raise any exceptions from the Salesforce class or otherwise
+        except Exception as e:
+            raise(e)
+            
         csv_data = [r for r in results]
 
         df_list = []
@@ -142,10 +115,11 @@ class sf_connect:
             #Append a dataframe with the values and columns to the df_list
             df_list.append(pd.DataFrame([row for row in reader if row], columns = [c for c in col_reader]))
         
-        #Concatenate the df_list
-        self.data = pd.concat(df_list)
-
-        #Map these data types to the appropriate pandas data types
-        self.to_dtypes = {k: DTYPE_MAPPER.get(v) for k,v in from_dtypes.items() if k in self.data.columns}
+        if df_list:
+            #Concatenate the df_list
+            self.data = pd.concat(df_list)
+    
+            #Map these data types to the appropriate pandas data types
+            self.to_dtypes = {k: DTYPE_MAPPER.get(v) for k,v in from_dtypes.items() if k in self.data.columns}
 
         #return data
